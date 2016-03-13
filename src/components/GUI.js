e59c2ed04a91ddd1947d9f8f1149d62a0970a558
Ludovic Barman <ludovic.barman@gmail.com>
2016-03-13 10:41:48 +0100

    Simple GUI updates

--- src/components/GUI.js
+++ src/components/GUI.js
@@ -12,16 +12,22 @@ export default class GUI extends Component {
 
     this.player = this.transform.parent.getComponent(Player);
     // This Component is not synchronized so we can do exotic shit in here
-    this.text = this.scene.newPrefab(GUIText, this).getComponent(Text);
-    this.text.entity.disableNetworking();
-    this.text.transform.localX = -9;
-    this.text.transform.localY = -10;
+    this.playerLife = this.scene.newPrefab(GUIText, this).getComponent(Text);
+    this.playerLife.entity.disableNetworking();
+    this.playerLife.transform.localX = -15;
+    this.playerLife.transform.localY = -10;
 
+    this.player = this.transform.parent.getComponent(Player);
+    // This Component is not synchronized so we can do exotic shit in here
+    this.playerMoney = this.scene.newPrefab(GUIText, this).getComponent(Text);
+    this.playerMoney.entity.disableNetworking();
+    this.playerMoney.transform.localX = 12;
+    this.playerMoney.transform.localY = -10;
 
-    this.t_nextWave = this.scene.newPrefab(GUIText, this).getComponent(Text);
-    this.t_nextWave.entity.disableNetworking();
-    this.t_nextWave.transform.localX = 0;
-    this.t_nextWave.transform.localY = -10;
+    this.timeBeforeNextWave = this.scene.newPrefab(GUIText, this).getComponent(Text);
+    this.timeBeforeNextWave.entity.disableNetworking();
+    this.timeBeforeNextWave.transform.localX = -15;
+    this.timeBeforeNextWave.transform.localY = 10.5;
 
     this.updateText();
   }
@@ -31,8 +37,16 @@ export default class GUI extends Component {
   }
 
   updateText() {
-    this.text.text = `Life: ${this.player.grid.hq.life}`;
-    this.t_nextWave.text = `Next wave in ${Math.floor(this.scene.controller.timeToNextWave / 1000)}`;
+    
+    let builder = "";
+    for(let i=0; i<this.player.grid.hq.life; i++)
+    {
+      builder = builder + "💗";
+    }
+
+    this.playerLife.text = builder;
+    this.playerMoney.text = `${this.player.money}$`;
+    this.timeBeforeNextWave.text = `Next wave in ${Math.floor(this.scene.controller.timeToNextWave / 1000)} sec`;
   }
 
   onDraw(ctx) {
